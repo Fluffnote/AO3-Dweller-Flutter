b7a400b7d34f8e5e2220badc097f6e18cbf143b9
fluffnote <blastnot3@gmail.com>
2026-08-29 23:03:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# AO3 Dweller (Flutter)
+
+AO3 Dweller rewritten in flutter because better compile and testing ability
+
+
---
+++ android/app/src/main/java/com/fluffnote/ao3_dweller_flutter/MainActivity.java
@@ -0,0 +1,6 @@
+package com.fluffnote.ao3_dweller_flutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
